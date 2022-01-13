db84017dfe92e2fecd98e7144e0919919f718c0f
williamcorwin <williamc400@nycstudents.net>
2022-01-13 13:55:31 -0500

    1

--- my-project/main.js
+++ my-project/main.js
@@ -1,6 +1,99 @@
 import './index.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1>Goodbye Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+let items_array = [
+  { "name": "carrots", "id": 1, count: 1 },
+  { "name": "spinach", "id": 2, count: 1 },
+  { "name": "cookies", "id": 3, count: 1 },
+  { "name": "lettuce", "id": 4, count: 1 },
+  { "name": "avocado", "id": 5, count: 1 }
+];
+
+let cart = [];
+
+function appendNode(parent, element) {
+  parent.appendChild(element);
+};
+
+function getDiv(container) {
+  return document.getElementById(container);
+};
+
+function createNode(node) {
+  let element = document.createElement(node);
+  return element;
+};
+
+function displayItems(items, container) {
+  let items_container = getDiv(container);
+  items_container.innerHTML = '';
+
+  for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+
+      let item_node = createNode("li");
+      item_node.setAttribute("id", item.id);
+
+      if (item.count > 0) {
+          item_node.innerHTML = `${item.name} 
+          <span id="badge">${item.count}</span>`;
+          appendNode(items_container, item_node);
+      }
+  }
+}
+
+items_container.innerHTML = '';
+
+displayItems(items_array, "items");
+
+function addOrRemoveItemsFromCart(action) {
+  let container = '';
+
+  if (action == "add") {
+      container = getDiv("items");
+
+      takeAction(container)
+  }
+  else if (action == "remove") {
+      container = getDiv("cart");
+
+      takeAction(container)
+  };
+}
+
+function addOrRemoveItemsFromCart(action) {
+  // code in the previous block is still there
+
+  function takeAction(container) {
+      container.addEventListener("click", function (event) {
+          let item_id = event.target.id;
+
+          if (item_id !== "items" && item_id !== "badge") {
+              let item = items_array.filter(function (item) {
+                  return item.id == item_id;
+              })[0];
+
+              let item_in_cart = cart.filter(function (item) {
+                  return item.id == item_id;
+              })[0];
+
+              if (item_in_cart == undefined) {
+                  cart.push(item);
+              } else if (action == "add") {
+                  item_in_cart.count++;
+              } else if (action == "remove") {
+                  item_in_cart.count--;
+              }
+
+              console.log(cart);
+              displayItems(cart, "cart");
+          };
+      });
+  };
+}
+
+addOrRemoveItemsFromCart('add');
+addOrRemoveItemsFromCart('remove');

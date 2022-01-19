9180b9ff13ff912a6bea9d7e7d96462288dca7da
williamcorwin <williamc400@nycstudents.net>
2022-01-19 13:56:47 -0500

    1

--- my-project/script.js
+++ my-project/script.js
@@ -1,6 +1,3 @@
-import "../styles/index.css";
-import "../styles/style.css";
-
 let items_array = [
   { "name": "carrots", "id": 1, count: 1 },
   { "name": "spinach", "id": 2, count: 1 },
@@ -56,6 +53,37 @@ function addOrRemoveItemsFromCart(action) {
       container = getDiv("cart");
 
       takeAction(container)
+  };
+}
+
+function addOrRemoveItemsFromCart(action) {
+  // code in the previous block is still there
+
+  function takeAction(container) {
+      container.addEventListener("click", function (event) {
+          let item_id = event.target.id;
+
+          if (item_id !== "items" && item_id !== "badge") {
+              let item = items_array.filter(function (item) {
+                  return item.id == item_id;
+              })[0];
+
+              let item_in_cart = cart.filter(function (item) {
+                  return item.id == item_id;
+              })[0];
+
+              if (item_in_cart == undefined) {
+                  cart.push(item);
+              } else if (action == "add") {
+                  item_in_cart.count++;
+              } else if (action == "remove") {
+                  item_in_cart.count--;
+              }
+
+              console.log(cart);
+              displayItems(cart, "cart");
+          };
+      });
   };
 }
 

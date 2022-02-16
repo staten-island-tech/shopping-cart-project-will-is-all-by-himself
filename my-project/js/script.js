d1a656fc1efb35f659419a14a46e6aeb9c358a8e
williamcorwin <williamc400@nycstudents.net>
2022-02-16 13:56:44 -0500

    123

--- my-project/js/script.js
+++
@@ -1,9 +0,0 @@
-import { store } from "./shop";
-console.log(store);
-const DOMSelectors = {
-    shop: document.querySelector('.storedisplay'),
-    add: document.getElementsByClassName('add-to-cart'),
-    cart: document.querySelector('.cart'),
-}
-
-
